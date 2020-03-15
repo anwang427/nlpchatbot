40b46564e71fa3ff7ba21b0b8568aec2b670ceee
TheHong <chcheung.office@gmail.com>
2020-03-14 23:58:08 -0400

    Data generation completed

--- data_innovation/data_gen.py
+++ data_innovation/data_gen.py
@@ -24,6 +24,9 @@ Assumptions made in this simulation:
 ^So in reality, there will be more complex mechanisms in play.
 """ 
 
+import numpy as np
+
+
 class SmsElements:
     queries = { # Frequencies out of 175
         "new crop production": 124,
@@ -37,15 +40,15 @@ class SmsElements:
     time = { # Key is the upperbound in hours. Value is the estimated probability. Hours in integers
         6: 0.05,
         12: 0.35,
-        18: 0.45
+        18: 0.45,
         23: 0.15
     }
     district = 36 # Number of districts in Maharashtra. Assuming equal probability for any of the 36
     season = {
-        'winter': 2/12,
-        'summer': 3/12,
-        'monsoon': 4/12
-        'post-monsoon': 3/12,
+        'winter': 0.17, #2/12,
+        'summer': 0.25, #3/12,
+        'monsoon': 0.33, #4/12,
+        'post-monsoon': 0.25 # 3/12
     }
 
     word_vector_length = len(queries) + 24 + district + len(season)
@@ -53,18 +56,17 @@ class SmsElements:
 
 def create_texts(num_texts):
     # Devising query probabilities
-    query_probs = np.array(list(queries.values())) / 175.
+    query_probs = np.array(list(SmsElements.queries.values())) / 175.
     query_probs_normalized = query_probs / sum(query_probs)
 
     # Devising time probabilities
-    time_probs = np.zeros(1, 24)
+    time_probs = np.zeros(24)
     for hour in range(24):
-        for upper_bound in SmsElements.time.keys():
+        for upper_bound in np.sort(list(SmsElements.time.keys())):
             if hour <= upper_bound:
                 time_probs[hour] = SmsElements.time[upper_bound]
                 break 
     time_probs = time_probs / sum(time_probs)
-
 
     # Creating word vectors
     word_vectors = []
@@ -76,19 +78,28 @@ def create_texts(num_texts):
         #         new_word = 1
 
         #(option 2)
-        query_vector = np.random.choice(np.eye(len(SmsElements.queries))), len(SmsElements.queries), p=query_probs_normalized)
+        num_queries = len(SmsElements.queries)
+        query_vector = np.eye(num_queries)[np.random.choice(num_queries, p=query_probs_normalized)]
+        # print("q", query_vector)
 
         # 2. Forming time
-        time_vector = np.random.choice(np.eye(len(time_probs)), len(time_probs), p=time_probs)
+        num_time = len(time_probs)
+        time_vector = np.eye(num_time)[np.random.choice(num_time, p=time_probs)]
+        # print("t", time_vector)
 
         # 3. Forming district
-        district_vector = np.random.choice(np.eye(SmsElements.district))
+        district_vector = np.eye(SmsElements.district)[np.random.choice(SmsElements.district)]
+        # print("d", district_vector)
 
         #4. Forming season
-        season_vector = np.random.choice(np.eye(len(SmsElements.season)), len(SmsElements.season), p=list(SmsElements.season.values()))
+        num_season = len(SmsElements.season)
+        season_vector = np.eye(num_season)[np.random.choice(num_season, p=list(SmsElements.season.values()))]
+        # print("s", season_vector)
 
         # 5. Combining vectors to form one word vector
         word_vectors.append(np.hstack([query_vector, time_vector, district_vector, season_vector]))
+    
+    return word_vectors
 
-
-create_texts(1)
+if __name__ == "__main__":
+    print(create_texts(1))
